2d20c36e732ace9b732df8ef8ad3889daa358167
Navnit Anuth <nanuth@qaworks.com>
2018-04-28 17:54:11 +0100

    Added MOQ

--- FriendStorage.UI.Tests/ViewModel/MainViewModelTests.cs
+++ FriendStorage.UI.Tests/ViewModel/MainViewModelTests.cs
@@ -1,4 +1,6 @@
-﻿using FriendStorage.UI.ViewModel;
+﻿using FriendStorage.UI.Contracts;
+using FriendStorage.UI.ViewModel;
+using Moq;
 using Xunit;
 
 namespace FriendStorage.UI.Tests.ViewModel
@@ -8,14 +10,16 @@ namespace FriendStorage.UI.Tests.ViewModel
         [Fact]
         public void ShouldCallTheLoadMethodOfTheNavigationViewModel()
         {
+            var navigationViewModelMock = new Mock<INavigationViewModel>();
+
             //// Arrange
-            //var mainViewModel = new MainViewModel();
+            var mainViewModel = new MainViewModel(navigationViewModelMock.Object);
 
             //// Act
-            //mainViewModel.Load();
+            mainViewModel.Load();
 
             //// Assert
-            
+            navigationViewModelMock.Verify(x => x.Load(), Times.Once);
         }
     }
 }
--- FriendStorage.UI.Tests/ViewModel/NavigationViewTests.cs
+++ FriendStorage.UI.Tests/ViewModel/NavigationViewTests.cs
@@ -1,17 +1,34 @@
-﻿using System.Linq;
-using FriendStorage.UI.Tests.Mocks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FriendStorage.Model;
+using FriendStorage.UI.DataProvider;
 using FriendStorage.UI.ViewModel;
+using Moq;
 using Xunit;
 
 namespace FriendStorage.UI.Tests.ViewModel
 {
     public class NavigationViewTests
     {
+        private readonly Mock<INavigationDataProvider> _navigationDataProviderMock;
+
+        public NavigationViewTests()
+        {
+            _navigationDataProviderMock = new Mock<INavigationDataProvider>();
+            _navigationDataProviderMock.Setup(x => x.GetAllFriends()).Returns(new List<FriendLookupItem>
+            {
+                new FriendLookupItem  { DisplayMember = "nAvz killa", Id = 1},
+                new FriendLookupItem { DisplayMember = "Neelam Paree", Id = 2}
+            });
+        }
+
         [Fact]
         public void ShouldLoadFriends()
         {
             // Arrange
-            var navigationViewModel = new NavigationViewModel(new NavigationDataProviderMock());
+             
+            
+            var navigationViewModel = new NavigationViewModel(_navigationDataProviderMock.Object);
 
             // Act
             navigationViewModel.Load();
@@ -28,7 +45,7 @@ namespace FriendStorage.UI.Tests.ViewModel
         public void ShouldLoadTestsOnlyOnce()
         {
             // Arrange
-            var navigationViewModel = new NavigationViewModel(new NavigationDataProviderMock());
+            var navigationViewModel = new NavigationViewModel(_navigationDataProviderMock.Object);
 
             // Act
             navigationViewModel.Load();
